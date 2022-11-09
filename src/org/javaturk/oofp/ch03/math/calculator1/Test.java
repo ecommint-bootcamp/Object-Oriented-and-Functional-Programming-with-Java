522bad6f53e2ddbd565b01c744172ca5e166807c
yabdioglu <yabdioglu37@gmail.com>
2022-11-09 21:44:30 +0300

    03-Arayüzler: 4

--- src/org/javaturk/oofp/ch03/math/calculator1/Test.java
+++ src/org/javaturk/oofp/ch03/math/calculator1/Test.java
@@ -8,9 +8,10 @@ public class Test {
 	public static void main(String[] args) {
 		in = new Scanner(System.in);
 		
-		CalculatorT1 calculator1 = new CalculatorT1(2);
+		CalculatorT1 calculator1 = new CalculatorT1(3);
 		calculator1.addFunction(new SinFunction());
 		calculator1.addFunction(new CosFunction());
+		calculator1.addFunction(new SquareFunction());
 //		calculator1.listMathFunction();
 		
 //		System.out.println();
